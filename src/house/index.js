5826624b1895f604be1d7a4f39cdb3dff6a33982
Bakhtiyar Zubairov <irookanji@gmail.com>
2021-11-20 23:25:28 +0100

    Add search results functionality and replace logo

--- src/house/index.js
+++ src/house/index.js
@@ -1,6 +1,13 @@
 import "./house.css";
+import { useState } from "react";
+import emailIcon from "./Email.png";
+import Inquiry from "./Inquiry";
 
 const House = ({ house }) => {
+  const [inquiryShown, setInquiryShown] = useState(false);
+  const inquiryClick = () => {
+    setInquiryShown(!inquiryShown);
+  };
   return (
     <div>
       <div className="row mt-2">
@@ -13,10 +20,17 @@ const House = ({ house }) => {
         <div className="col-md-7">
           <img src={`/images/${house.photo}.jpeg`} alt="House" />
         </div>
-      </div>
-      <div className="col-md-5">
-        <p className="price">${house.price}</p>
-        <p>{house.description}</p>
+        <div className="col-md-5">
+          <p className="price">${house.price}</p>
+          <p>{house.description}</p>
+          <img
+            src={emailIcon}
+            height="50"
+            alt="inquiry"
+            onClick={inquiryClick}
+          />
+          {inquiryShown && <Inquiry house={house} />}
+        </div>
       </div>
     </div>
   );
